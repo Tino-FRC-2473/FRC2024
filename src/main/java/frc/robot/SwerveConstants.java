9f136021bd3591846bbf7ff8ec09ba08b61aaacd
Mira <mpanigrahy607@student.fuhsd.org>
2024-01-18 19:21:32 -0800

    tested path 2, added turn movement for paths

--- src/main/java/frc/robot/SwerveConstants.java
+++ src/main/java/frc/robot/SwerveConstants.java
@@ -124,7 +124,7 @@ public final class SwerveConstants {
 	}
 
 	public static final class AutoConstants {
-		public static final double MAX_SPEED_METERS_PER_SECOND = 0.5; // 0.5 decided
+		public static final double MAX_SPEED_METERS_PER_SECOND = 0.3; // 0.5 decided
 		public static final double MAX_ANGULAR_SPEED_RADIANS_PER_SECOND = Math.PI / 12;
 		public static final double MAX_ANGULAR_SPEED_RADIANS_PER_SECOND_SQUARED = Math.PI / 2;
 
