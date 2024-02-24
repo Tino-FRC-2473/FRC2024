8fae0ddac309b0e3006101b770a0e1f694e61fb3
Andrew Kwak <105023049+alienkwak@users.noreply.github.com>
2024-02-24 13:49:01 -0800

    Add files via upload

---
+++ Gui.java
@@ -0,0 +1,205 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+import java.lang.Math;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.MjpegServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.util.PixelFormat;
+import edu.wpi.first.cscore.CvSink;
+
+import edu.wpi.first.cscore.CvSource;
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+
+
+  public int speed;
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    CameraServer.startAutomaticCapture();
+    m_robotContainer = new RobotContainer();
+    
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  /** This function is called periodically during operator control. */
+
+
+
+  //This is my method now >:3
+  //Just put in the code in here and make a good GUI and then later run this by right clicking build.gradle
+  //selecting build.gradle (after connecting to a robot wifi) and then ask someone else what to do later
+  @Override
+  public void teleopPeriodic() {
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //Intake state of the robot
+    boolean intakeFilled=true;
+    SmartDashboard.putBoolean("Intake Full",intakeFilled);
+
+
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //X and Y coordinate of the robot
+    double xValue=69;//Remove hard coding and replace with status of the robot
+    double yValue=69;//Remove hard coding and replace with status of the robot
+    SmartDashboard.putString("X Coord",Double.toString(xValue));
+    SmartDashboard.putString("Y Coord",Double.toString(yValue));
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //CV alignment state (Very important)
+    boolean isAlligned=false;//Remove hard coding and replace with status of the robot
+    isAlligned=Math.random()<0.5;
+    int randomInt= (int)(Math.random()*90);
+    SmartDashboard.putBoolean("CV Locked",isAlligned);
+    SmartDashboard.putString("Random Number",Integer.toString(randomInt));
+
+
+    //BY THE WAY THE OTHER COMPUTER IS COMPUTER 5
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //Speed of Robot
+    speed +=5;
+    if(speed>95){
+      speed=0;
+    }
+    SmartDashboard.putNumber("Speed of Robot:",speed);
+
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //Autonomous state it is in
+    String autoState="John Doe";//Remove hard coding and replace with status of the robot
+    SmartDashboard.putString("Autonomous State",autoState);
+
+
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //Selection tool for autonomous state
+
+
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //Camera Feed
+
+
+    // Creates the CvSink and connects it to the UsbCamera
+
+    // Creates the CvSource and MjpegServer [2] and connects them
+
+
+
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //Mechanical Launcher State
+
+    String nechState="Launching";//Remove hard coding and replace with status of the robot
+    SmartDashboard.putString("MechState",nechState);
+
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+    //Robot Angle (Might be too hard, but would calculate what angle the robot was at relative to starting position)
+    int angle=speed;
+    SmartDashboard.putNumber("Angle of Robot:",angle);
+
+
+
+
+    //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+
+
+
+
+
+
+  }
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
